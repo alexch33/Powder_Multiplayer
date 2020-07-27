c374c550b9cb0574f3eef54f468f8b31d6379c55
i3po <lexa288LEXA288>
2020-07-27 20:25:43 +0300

    players_sync

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerControls : MonoBehaviour
+{
+    private PhotonView photonView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.IsMine) return;
+        
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-Time.deltaTime * 5, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(Time.deltaTime * 5, 0, 0);    
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,12 @@ using Photon.Realtime;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public GameObject PlayerPrefab;
     // Start is called before the first frame update
     void Start()
     {
-
+        Vector3 pos = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        PhotonNetwork.Instantiate(PlayerPrefab.name, pos, Quaternion.identity);
     }
 
     // Update is called once per frame
